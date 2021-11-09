640083f05629867cd32a37ca6d551fb1ec7810f4
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2021-11-09 23:47:12 +0000

    Adição explicação constante

---
+++ Constants and Variables/constant.swift
@@ -0,0 +1,8 @@
+/* uma constante ao receber um valor, não poderá mais receber um outro valor. 
+Ao tentar alterar o valor de uma constante, será exibido um erro.
+Obs.:O swift utiliza o LET como constante.
+ex:
+*/ 
+
+let nome = "Paulo"
+print(nome)
