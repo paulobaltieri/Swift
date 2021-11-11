59af9789123961e14a8aaac2dfd1e5ab919de4b7
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2021-11-11 00:19:00 +0000

    Tipos de variaveis

---
+++ Tipos de variaveis/tipos.swift
@@ -0,0 +1,66 @@
+  /* Existem 4 tipos de variaveis: String, int, float,boolean, double */
+
+/*String: Esse tipo de variavel receberá apenas valores do tipo texto. 
+
+  Int: Esse tipo de variavel receberá apenas valoros do tipo inteiro.
+
+  Float: Variaveis do tipo float recebem apenas numeros decimais, mas pelo fato de ser limitado, não é muito utilizado.
+
+  Boolean: Esse tipo de variavel recebe apenas 1 de dois valores, True ou False.
+
+  Double: Essa variavel tambem trabalha com numeros deciamsi, mas pelo fato de possuir um numero de casas decimais maior, é o mais utlizado.
+
+Obs: dependendo do valor, o swift consegue determinar o tipo de variavel que você deseja trabalhar.
+
+    - tipo String
+    let nome = "Paulo"
+    let nome :String = "Paulo"
+
+    - tipo Int
+    let idade = 32
+    let idade :Int = 32
+
+    - tipo Float
+    let valor = 3.50
+    let valor: Float = 3,50
+
+    - tipo Double
+    let pi = 3.5454854845418484
+    let pi :Double = 3.5454854845418484
+*/
+
+let explicacao = """
+  Existem 4 tipos de variaveis: String, int, float,boolean, double.
+
+  String: Esse tipo de variavel receberá apenas valores do tipo texto
+
+  Int: Esse tipo de variavel receberá apenas valoros do tipo inteiro.
+
+  Float: Variaveis do tipo float recebem apenas numeros decimais, mas pelo fato de ser limitado, não é muito utilizado.
+
+  Boolean: Esse tipo de variavel recebe apenas 1 de dois valores, True ou False.
+
+  Double: Essa variavel tambem trabalha com numeros deciamsi, mas pelo fato de possuir um numero de casas decimais maior, é o mais utlizado.
+
+  Obs: dependendo do valor, o swift consegue determinar o tipo de variavel que você deseja trabalhar.
+
+      - tipo String
+    let nome = "Paulo"
+    let nome :String = "Paulo"
+
+    - tipo Int
+    let idade = 32
+    let idade :Int = 32
+
+    - tipo Float
+    let valor = 3.50
+    let valor: Float = 3,50
+
+    - tipo Double
+    let pi = 3.5454854845418484
+    let pi :Double = 3.5454854845418484
+
+
+"""
+print(explicacao)  
+  
